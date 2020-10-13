7003b167cef7595b82c77ff4ef24972f00412040
Neodevcl <neodevchile@gmail.com>
2020-10-13 18:54:14 -0300

    Updates

---
+++ precache-manifest.0ed488283c41ed67bc1e17504f52c09e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a8abc55958739b106e9faf57fba0af",
+    "url": "/country-app/index.html"
+  },
+  {
+    "revision": "5b8989c8e65f3fc90bc1",
+    "url": "/country-app/static/css/main.a30ca045.chunk.css"
+  },
+  {
+    "revision": "0dbfa7be3e5a6c4b078f",
+    "url": "/country-app/static/js/2.ae7e2429.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-app/static/js/2.ae7e2429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8989c8e65f3fc90bc1",
+    "url": "/country-app/static/js/main.9d973389.chunk.js"
+  },
+  {
+    "revision": "a2cb7f73a4431512e5dc",
+    "url": "/country-app/static/js/runtime-main.a5ec798a.js"
+  }
+]);
